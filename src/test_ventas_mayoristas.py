d9616224c36c85444eff0f956a4af5ae982a4543
Sharay07 <shaporjur@alum.us.es>
2021-10-21 18:33:51 +0200

    Continuación proyecto.

--- src/test_ventas_mayoristas.py
+++ src/test_ventas_mayoristas.py
@@ -1,8 +1,18 @@
-from ventas_mayoristas import *  #importa el método que he creado
+from ventas_mayoristas import *  
 
 def main():
     datos = lee_ventas_mayoristas('data/ventas_mayorista.csv')
-    print("Se han cargado", len(datos), "registros.")
+    print("Se han cargado", len(datos), "registros de ventas mayoristas.")
+    print("")
+
+    print("Mostrando los 3 primeros:")
+    for r in datos[:3]:
+        print("\t", r)
+    print("")
+
+    print("Mostrando los 3 últimos:")
+    for s in datos[-3:]:
+        print("\t", s)
     
 if __name__ == '__main__': 
     main()
--- src/ventas_mayoristas.py
+++ src/ventas_mayoristas.py
@@ -1,5 +1,6 @@
 import csv
 from collections import namedtuple
+from datetime import datetime
 
 VentasMayoristas =  namedtuple('VentasMayoristas', 'medio_pedido, tipo_tienda, tipo_producto, \
                             producto, fecha_compra, unidades_venta, \
@@ -9,11 +10,13 @@ def lee_ventas_mayoristas(fichero):
     
     with open(fichero, encoding='utf-8') as f:
         
-        lector = csv.reader(f)
+        lector = csv.reader(f , delimiter=";")    
         next(lector)
         registro=[]
-        for medio_pedido, tipo_tienda, tipo_producto, producto, fecha_compra, unidades_venta, precio_unidad, margen_de_ganancia, libre_de_impuestos in lector:
-
+        for medio_pedido, tipo_tienda, tipo_producto, producto, fecha_compra, unidades_venta, \
+            precio_unidad, margen_de_ganancia, libre_de_impuestos in lector:
+            
+            fecha_compra = datetime.strptime(fecha_compra, "%d/%m/%Y").date()
             unidades_venta = int(unidades_venta)
             precio_unidad = float(precio_unidad)
             margen_de_ganancia = float(margen_de_ganancia)
